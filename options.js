fb489bde6e5c17c29f5f9b597dac05be958e0d6f
Daniel Vena <deaconvena+gitbox@gmail.com>
2013-01-08 17:44:36 -0500

    started add url functionality

--- options.js
+++ options.js
@@ -20,7 +20,22 @@ function restore_options()
 
 	intervalEl.addEventListener('keyup', save_options);
 
+	var add_url_buttons = document.getElementsByClassName('add_a_url');
+	for(var i = 0, j = add_url_buttons.length; i < j; i++) {
+		add_url_buttons[i].addEventListener('click', append_url_template);
+	}
+
 	save_options();
+}
+
+function append_url_template()
+{
+	console.log('appending a new url template');
+	var template = document.querySelector('.url_entry.template').cloneNode(1),
+		container = document.getElementById('urls_container');
+
+	template.className = template.className.replace(/template/, '');
+	container.appendChild(template);
 }
 
 document.addEventListener('DOMContentLoaded', restore_options);
