609535d9609a895040dd48f59669c4c5001eefa3
Daniel Vena <deaconvena+github@gmail.com>
2013-01-09 10:50:21 -0500

    urls are now saved and restored

--- options.js
+++ options.js
@@ -11,10 +11,20 @@ function save_options()
 	}
 
 	// save each url entry
-	var url_entries = urls_container.getElementsByClassName('url_entry');
+	var url_entries = urls_container.getElementsByClassName('url_entry'),
+		urls = [];
 	for (var i = 0, j = url_entries.length; i < j; i++) {
-
+		var url_entry = url_entries[i];
+		var title = url_entry.querySelector('.title input'),
+			url = url_entry.querySelector('.url input');
+		if ((title || url) && (title.value || url.value)) {
+			urls.push({
+				"title": title.value || "",
+				"url": url.value || ""
+			});
+		}
 	}
+	localStorage['urls'] = JSON.stringify(urls);
 
 }
 
@@ -29,10 +39,7 @@ function restore_options()
 	intervalEl.value = interval || 3600;
 
 	for (i = 0, j = urls.length; i < j; i++) {
-		template = document.querySelector('.url_entry.template').cloneNode(1);
-		template.querySelector('.call_letters input').value = urls[i].call_letters;
-		template.querySelector('.url input').value = urls[i].url;
-		urls_container.appendChild(template);
+		append_url_template(urls[i].title, urls[i].url);
 	}
 
 	var add_url_buttons = document.getElementsByClassName('add_a_url');
@@ -40,13 +47,16 @@ function restore_options()
 		add_url_buttons[i].addEventListener('click', append_url_template);
 	}
 
+	document.getElementById('loading').style.display = 'none';
 }
 
-function append_url_template()
+function append_url_template(t, u)
 {
-	console.log('appending a new url template');
 	var template = document.querySelector('.url_entry.template').cloneNode(1),
 		container = document.getElementById('urls_container');
+
+	template.querySelector('.title input').value = typeof t != 'object' ? t : '';
+	template.querySelector('.url input').value = u || '';
 
 	template.className = template.className.replace(/template/, '');
 	container.appendChild(template);
