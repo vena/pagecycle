4797438830adc7956bad066c546024004e752172
Daniel Vena <deaconvena+github@gmail.com>
2013-01-09 22:10:33 -0500

    added init for interval

--- extension/initdb.js
+++ extension/initdb.js
@@ -4,3 +4,8 @@ var PCDB = WebSQL('pagecycle');
 PCDB.query(
 	'CREATE TABLE IF NOT EXISTS urls (id INTEGER PRIMARY KEY AUTOINCREMENT, sort INTEGER, title TEXT, url TEXT)'
 );
+
+// initialise interval
+if (!$.isNumeric(localStorage.interval)) {
+	localStorage.interval = 60*1000;
+}
